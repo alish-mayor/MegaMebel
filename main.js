8c38f4824bc5da42fbd01423b46d1fe14d62cb31
alish-mayor <alish22061999@gmail.com>
2021-09-13 01:14:26 +0600

    gallery slider features implemented

--- main.js
+++ main.js
@@ -4,6 +4,21 @@ const burgerMenu = document.querySelector(".burger-menu");
 const menuLinks = document.querySelectorAll(".menu-links");
 const btn = document.querySelector(".btn");
 const form = document.querySelector(".form__container");
+const leftArrow = document.querySelector(".left-arrow");
+const rightArrow = document.querySelector(".right-arrow");
+const galleryImg = document.querySelector(".gallery-img");
+const dots = document.querySelectorAll(".dot");
+const images = [
+  "6.jpg",
+  "12.jpg",
+  "30-1.jpg",
+  "79b757f9c3e40ebe2acb0b3ceb1c885aa515e4fa_optimizm.jpg",
+  "15737.750@2x.jpg",
+  "dsv-stenka-ronda-3_enl.jpg",
+  "Mebel.jpg",
+  "kuhonnaya-mebel.jpg",
+];
+let curIndex = 0;
 
 const openMenu = function () {
   burgerMenu.style.transform = "translateX(0)";
@@ -13,6 +28,13 @@ const closeMenu = function () {
   burgerMenu.style.transform = "translateX(100%)";
 };
 
+const renderImageAndChangeDot = function (index) {
+  galleryImg.setAttribute("src", `assets/images/${images[index]}`);
+  dots.forEach((dot) => {
+    dot.classList.remove("dot_active");
+  });
+  dots[index].classList.add("dot_active");
+};
 // Event listeners
 
 openMenuBtn.addEventListener("click", openMenu);
@@ -42,3 +64,27 @@ form.addEventListener("submit", function (e) {
   console.log(document.querySelector(".input-name").value);
   console.log(document.querySelector(".input-phone").value);
 });
+
+leftArrow.addEventListener("click", function () {
+  if (curIndex === 0) {
+    curIndex = images.length - 1;
+  } else {
+    curIndex--;
+  }
+
+  renderImageAndChangeDot(curIndex);
+});
+
+rightArrow.addEventListener("click", function () {
+  if (curIndex === images.length - 1) {
+    curIndex = 0;
+  } else {
+    curIndex++;
+  }
+
+  renderImageAndChangeDot(curIndex);
+});
+
+document.addEventListener("DOMContentLoaded", () =>
+  galleryImg.setAttribute("src", `assets/images/${images[curIndex]}`)
+);
